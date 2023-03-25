7adc5f17f2df6691358093968649efb3e49ed642
NataliaJanda <jkl.natalia1989@gmail.com>
2023-03-25 19:50:10 +0100

    Changes

---
+++ server/src/main/java/com/prz/Projektbdio/ProjektBdioApplication.java
@@ -0,0 +1,13 @@
+package com.prz.Projektbdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBdioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBdioApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/prz/Projektbdio/ProjektBdioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prz.Projektbdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektBdioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
