5b896e4304dbf5920392521919cdcd80ff7455b9
KacperJarocki <kacper.jarocki1121@gmail.com>
2023-04-09 22:30:54 +0200

    Cross orgin enabled

--- server/src/main/java/Projektbdio/controller/AccountsController.java
+++ server/src/main/java/Projektbdio/controller/AccountsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequiredArgsConstructor
 public class AccountsController {
     public final AccountsService accountsService;
