031c33a8199fdee86aeb4d71ceeb5a44cccfcf43
KacperJarocki <kacper.jarocki1121@gmail.com>
2023-04-11 21:09:59 +0200

    updated category entity, added method to category repo

--- server/src/main/java/Projektbdio/model/Category.java
+++ server/src/main/java/Projektbdio/model/Category.java
@@ -4,13 +4,15 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Table(name="Category")
 @Getter
 @Setter
 public class Category {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int category_id;
     private String name;
+
 }
---
+++ server/src/main/java/Projektbdio/repository/CategoryRepository.java
@@ -0,0 +1,11 @@
+package Projektbdio.repository;
+
+import Projektbdio.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+@Repository
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    Optional<Category> findByName(String name);
+}
