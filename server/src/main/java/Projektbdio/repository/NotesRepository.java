9b46766801e2324806849e64d6ebb3d72bfcc871
bajkacper <bajkacper1@gmail.com>
2023-04-29 22:55:57 +0200

    admin bugfix

--- server/src/main/java/Projektbdio/repository/NotesRepository.java
+++ server/src/main/java/Projektbdio/repository/NotesRepository.java
@@ -1,7 +1,15 @@
 package Projektbdio.repository;
 
 import Projektbdio.model.Notes;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.transaction.Transactional;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +18,6 @@ import java.util.List;
 public interface NotesRepository extends JpaRepository< Notes , Integer> {
 
     List<Notes> findNotesByAccounts_NameUser(String name);
-    //void deleteNotesByAccounts_Account_id(int id);
-
+    @Transactional
     void deleteNotesByAccounts_AccountId(int id);
-
 }
