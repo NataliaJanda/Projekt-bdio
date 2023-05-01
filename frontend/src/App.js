afe989a84a679ab741c4ca75f14ed2c9c659b68f
MrFlamePL <asdfghjui1@gmail.com>
2023-05-01 23:51:27 +0200

    added proper app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Register from "./components/Register/Register";
 import LoginPage from "./components/Login/LoginPage";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import AdminPage from "./components/AdminPage/AdminPage";
+import EditUser from "./components/AdminPage/EditUser";
+import AddUser from "./components/AdminPage/AddUser";
+import Home from "./components/AdminPage/Home";
+
 
 
 const App = () => {
@@ -20,6 +24,9 @@ const App = () => {
           <Route path = "/components/Register" element = {loggedIn ? <DashboardSite/> :<Register />}/>
           <Route path = "/components/Login" element = {loggedIn ? <DashboardSite/> :<LoginPage />}/> 
           <Route path = "/components/AdminPage" element = {loggedIn ? <AdminPage/>:<AdminPage/>}/>
+          <Route path = "/components/EditUser" element = {loggedIn ? <EditUser/>:<EditUser/>}/>
+          <Route path = "/components/AddUser" element = {loggedIn ? <AddUser/>:<AddUser/>}/>
+          <Route path = "/components/Home" element = {loggedIn ? <Home/>:<Home/>}/>
        </Routes>
       </Router>
  
